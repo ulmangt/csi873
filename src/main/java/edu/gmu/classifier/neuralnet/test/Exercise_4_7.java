f79697d33a2fe0b391e0acf5219b099ef622c671
geoff.ulman <geoff.ulman@60d4b78f-f32b-2441-5510-30ef7776ec13>
2011-10-02 06:08:14 +0000

    tested on homework problem.

--- src/main/java/edu/gmu/classifier/neuralnet/test/Exercise_4_7.java
+++ src/main/java/edu/gmu/classifier/neuralnet/test/Exercise_4_7.java
@@ -1,10 +1,14 @@
 package edu.gmu.classifier.neuralnet.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import edu.gmu.classifier.neuralnet.factory.NetFactory;
 import edu.gmu.classifier.neuralnet.net.Net;
 import edu.gmu.classifier.neuralnet.node.NodeFunctions;
+import edu.gmu.classifier.neuralnet.train.Backpropagation;
+import edu.gmu.classifier.neuralnet.train.Backpropagation.TrainingExample;
 
 public class Exercise_4_7
 {
@@ -14,6 +18,17 @@ public class Exercise_4_7
 	
 		net.apply( NodeFunctions.setWeights( 0.1 ) );
 	
-		System.out.println( Arrays.toString( net.calculateOutput( 0.0, 0.0 ) ) );
+		System.out.println( Arrays.toString( net.calculateOutput( 1.0, 0.0 ) ) );
+		
+		Backpropagation b = new Backpropagation( );
+		
+		List<TrainingExample> training = new ArrayList<TrainingExample>( );
+		training.add( new TrainingExample( new double[] { 1, 0 }, new double[] { 1 } ) );
+		training.add( new TrainingExample( new double[] { 0, 1 }, new double[] { 0 } ) );
+		
+		b.train( net, training, 0.3, 0.9 );
+		
+		System.out.println( Arrays.toString( net.calculateOutput( 1.0, 0.0 ) ) );
+		System.out.println( Arrays.toString( net.calculateOutput( 0.0, 1.0 ) ) );
 	}
 }
