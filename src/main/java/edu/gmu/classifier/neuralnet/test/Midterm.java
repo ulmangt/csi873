17977bc909bd0b9c1a09316308038b227cb2846c
geoff.ulman <geoff.ulman@60d4b78f-f32b-2441-5510-30ef7776ec13>
2011-10-17 00:52:21 +0000

    code cleanup, commenting, and additional test runs.

--- src/main/java/edu/gmu/classifier/neuralnet/test/Midterm.java
+++ src/main/java/edu/gmu/classifier/neuralnet/test/Midterm.java
@@ -6,19 +6,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JFrame;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.DefaultXYDataset;
-
 import edu.gmu.classifier.neuralnet.factory.NetFactory;
 import edu.gmu.classifier.neuralnet.io.DataLoader;
 import edu.gmu.classifier.neuralnet.net.Net;
-import edu.gmu.classifier.neuralnet.node.Link;
-import edu.gmu.classifier.neuralnet.node.Node;
 import edu.gmu.classifier.neuralnet.node.NodeFunctions;
 import edu.gmu.classifier.neuralnet.train.Backpropagation;
 import edu.gmu.classifier.neuralnet.train.Backpropagation.TrainingExample;
@@ -69,76 +59,14 @@ public class Midterm
 			trainingDataList.addAll( DataLoader.loadFile( new File( dataDirectory, fileName ) ) );
 		}
 		
-		Backpropagation b = new Backpropagation( );
-		
-		/* 32% error
-		Net net = NetFactory.newNet( 64, 40, 10 );
-		net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-		b.train( net, trainingDataList, testDataList, 0.3, 0.0 );
-		*/
-		
-		/* 40% error (!!!) -- no hidden layers actually does better than a few hidden layers
-		Net net = NetFactory.newNet( 64, 10 );
-		net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-		b.train( net, trainingDataList, testDataList, 0.3, 0.0 );
-		*/
-		
-		/*
-		Net net = NetFactory.newNet( 64, 20, 10 );
-		net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-		b.train( net, trainingDataList, testDataList, 0.3, 0.0 );
-		*/
-		
-		/*
-		Net net = NetFactory.newNet( 64, 10, 10 );
-		net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-		b.train( net, trainingDataList, testDataList, 0.3, 0.0 );
-		*/
-		
-		/*
-		Net net = NetFactory.newNet( 64, 2, 10 );
-		net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-		b.train( net, trainingDataList, testDataList, 0.3, 0.0 );
-		*/
-		
-		/*
-		Net net = NetFactory.newNet( 64, 4, 10 );
-		net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-		b.train( net, trainingDataList, testDataList, 0.3, 0.0 );
-		*/
-		
-		/*
+		// create a network
 		Net net = NetFactory.newNet( 64, 3, 10 );
+		
+		// randomize the network weights
 		net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-		b.train( net, trainingDataList, testDataList, 0.3, 0.0 );
-		*/
 		
-		// create jfreechart dataset for plotting purposes
-		DefaultXYDataset dataset2 = new DefaultXYDataset( );
-		
-		int steps = 30;
-		double stepd = 0.9 / steps;
-		
-		double[][] seriesData = new double[2][steps];
-		
-		for ( int i = 0 ; i < steps ; i++ )
-		{
-			Net net = NetFactory.newNet( 64, 4, 10 );
-			net.apply( NodeFunctions.setRandomWeights( -0.1, 0.1 ) );
-			b.train( net, trainingDataList, testDataList, 1.0 - i * stepd, 0.3 );
-			
-				seriesData[0][i] = 1.0 - stepd * i;
-				seriesData[1][i] = b.calculateError( net, testDataList );
-		}
-		
-		dataset2.addSeries( "Learning Rate", seriesData );
-		
-		JFreeChart chart2 = ChartFactory.createXYLineChart( "Test Error For Varying Learning Rate Parameters", "Learning Rate", "Error", dataset2, PlotOrientation.VERTICAL, false, false, false );
-		ChartPanel chartPanel2 = new ChartPanel( chart2 );
-		JFrame frame2 = new JFrame( );
-		frame2.setSize( 1000, 1000 );
-		frame2.add( chartPanel2 );
-		frame2.setVisible( true );
-		
+		// run the backpropagation algorithm to train the network using the above training examples
+		Backpropagation b = new Backpropagation( );
+		b.train( net, trainingDataList, testDataList, 0.15, 0.3 );
 	}
 }
