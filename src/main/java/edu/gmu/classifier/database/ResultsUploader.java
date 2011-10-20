6e38677a8ba628470363643ebeb412c8d718014d
geoff.ulman <geoff.ulman@60d4b78f-f32b-2441-5510-30ef7776ec13>
2011-10-20 00:32:28 +0000

    results are uploaded to database for visualization.

--- src/main/java/edu/gmu/classifier/database/ResultsUploader.java
+++ src/main/java/edu/gmu/classifier/database/ResultsUploader.java
@@ -11,7 +11,17 @@ public class ResultsUploader
 	public static final int IX_TRAIN_FIRST_INDEX = 171585;
 	public static final int IX_TEST_FIRST_INDEX = 171175;
 	
-	public void uploadResults( Net net, String description, List<TrainingExample> list, int firstDataId )
+	public static void uploadTrainingResults( Net net, List<TrainingExample> list, String description )
+	{
+		uploadResults( net, list, description, IX_TRAIN_FIRST_INDEX );
+	}
+	
+	public static void uploadTestingResults( Net net, List<TrainingExample> list, String description )
+	{
+		uploadResults( net, list, description, IX_TEST_FIRST_INDEX );
+	}
+	
+	public static void uploadResults( Net net, List<TrainingExample> list, String description, int firstDataId )
 	{
 		UploadRunQuery uploadRunQuery = new UploadRunQuery( description, System.currentTimeMillis( ) );
 		uploadRunQuery.runQuery( );
