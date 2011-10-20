a8ef0c010b9bd28338f3c16bd7c11f26ddac0f91
geoff.ulman <geoff.ulman@60d4b78f-f32b-2441-5510-30ef7776ec13>
2011-10-20 00:14:45 +0000

    refactored more utility code into NeuralNetUtils class.

--- trunk/src/main/java/edu/gmu/classifier/neuralnet/util/NeuralNetUtils.java
+++ trunk/src/main/java/edu/gmu/classifier/neuralnet/util/NeuralNetUtils.java
@@ -1,8 +1,12 @@
 package edu.gmu.classifier.neuralnet.util;
 
+import java.util.Arrays;
 import java.util.List;
 
 import edu.gmu.classifier.neuralnet.net.Net;
+import edu.gmu.classifier.neuralnet.node.Link;
+import edu.gmu.classifier.neuralnet.node.Node;
+import edu.gmu.classifier.neuralnet.node.NodeFunction;
 import edu.gmu.classifier.neuralnet.train.Backpropagation.TrainingExample;
 
 public class NeuralNetUtils
@@ -51,5 +55,38 @@ public class NeuralNetUtils
 		}
 		
 		return index;
+	}
+	
+	/**
+	 * Prints the iteration number and training example information and the current weights of the network.
+	 * 
+	 * @param iteration
+	 * @param net
+	 * @param data
+	 */
+	public static void printWeights( int iteration, final Net net, TrainingExample data )
+	{
+		System.out.printf( "Iteration %d Training Example %s %s%n", iteration, Arrays.toString( data.getInputs( ) ), Arrays.toString( data.getOutputs( ) ) );
+		printWeights( net );
+	}
+	
+	/**
+	 * Prints the current weights of each link in the current network.
+	 * 
+	 * @param net
+	 */
+	public static void printWeights( final Net net )
+	{
+		net.apply( new NodeFunction( )
+		{
+			@Override
+			public void run( Node node )
+			{
+				for ( Link link : node.getInputLinks( ) )
+				{
+					System.out.printf( "%s,%s,%.5f%n", link.getInputNode( ), link.getOutputNode( ), link.getWeight( ) );
+				}
+			}
+		});
 	}
 }
