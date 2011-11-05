6bc957cc2630aa8d7419619553502a72dab89c03
geoff.ulman <geoff.ulman@60d4b78f-f32b-2441-5510-30ef7776ec13>
2011-11-05 03:03:36 +0000

    naivebayes: added report files.

--- src/main/java/edu/gmu/classifier/database/ResultsUploader.java
+++ src/main/java/edu/gmu/classifier/database/ResultsUploader.java
@@ -1,8 +1,11 @@
 package edu.gmu.classifier.database;
 
 import java.util.List;
+import java.util.Map;
 
 import edu.gmu.classifier.io.TrainingExample;
+import edu.gmu.classifier.naivebayes.Homework7;
+import edu.gmu.classifier.naivebayes.P;
 import edu.gmu.classifier.neuralnet.net.Net;
 import edu.gmu.classifier.neuralnet.util.NeuralNetUtils;
 
@@ -37,6 +40,34 @@ public class ResultsUploader
 			TrainingExample data = list.get( i );
 			
 			double[] output = net.calculateOutput( data.getInputs( ) );
+			String sClassification = String.valueOf( NeuralNetUtils.getLargestIndex( output ) );
+			
+			UploadResultQuery uploadResultQuery = new UploadResultQuery( firstDataId + i, ixRunId, sClassification );
+			uploadResultQuery.runQuery( );
+		}
+	}
+	
+	public static void uploadTrainingResults( Map<P, Double> p0map, List<TrainingExample> list, String description )
+	{
+		uploadResults( p0map, list, description, IX_TRAIN_FIRST_INDEX );
+	}
+	
+	public static void uploadTestingResults( Map<P, Double> p0map, List<TrainingExample> list, String description )
+	{
+		uploadResults( p0map, list, description, IX_TEST_FIRST_INDEX );
+	}
+	
+	public static void uploadResults( Map<P, Double> p0map, List<TrainingExample> list, String description, int firstDataId )
+	{
+		UploadRunQuery uploadRunQuery = new UploadRunQuery( description, System.currentTimeMillis( ) );
+		uploadRunQuery.runQuery( );
+		int ixRunId = uploadRunQuery.getRunId( );
+		
+		for ( int i = 0 ; i < list.size( ); i++ )
+		{
+			TrainingExample data = list.get( i );
+			
+			double[] output = Homework7.calculateOutputLikelihoods( p0map, data );
 			String sClassification = String.valueOf( NeuralNetUtils.getLargestIndex( output ) );
 			
 			UploadResultQuery uploadResultQuery = new UploadResultQuery( firstDataId + i, ixRunId, sClassification );
