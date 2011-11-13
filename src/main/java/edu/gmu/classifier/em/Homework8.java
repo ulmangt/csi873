2995e26c60f61afe2b518adeda821bfe6aa7c98c
geoff.ulman <geoff.ulman@60d4b78f-f32b-2441-5510-30ef7776ec13>
2011-11-13 21:49:45 +0000

    added homework 8 files.

--- src/main/java/edu/gmu/classifier/em/Homework8.java
+++ src/main/java/edu/gmu/classifier/em/Homework8.java
@@ -13,6 +13,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.xy.DefaultXYDataset;
 
 public class Homework8
@@ -20,34 +21,43 @@ public class Homework8
 	public static void main( String args[] ) throws IOException
 	{
 		//load 2000 examples from the data file
-		double[] data = loadDataFile( "/home/ulman/CSI873/lec9/Fatena.txt" );
+		double[] data = loadDataFile( "/home/ulman/CSI873/lec9/Geoffrey.txt" );
+		
 		// get the number of data elements loaded from the file
 		int size = data.length;
+		
 		// create an array to store the estimated zj values for each data element
 		double[] z1 = new double[size];
 		double[] z2 = new double[size];
+		
 		// initialize the distribution means to 0
-		double u1 = Math.random( ) * 2 - 1;
-		double u2 = Math.random( ) * 2 - 1;
-		// the assumed sigma for both disributions is 1;
+		double u1 = Math.random( ) * 6 - 3;
+		double u2 = Math.random( ) * 6 - 3;
+
+		// the assumed sigma for both distributions is 1;
 		double sigma = 1;
 
+		// run for a fixed number of iterations
 		int iterations = 30;
 
+		// allocate memory for storing u1 and u2 values at each iteration (for plotting)
 		double[] u1l = new double[iterations];
 		double[] u2l = new double[iterations];
 
+		// run the EM algorithm
 		for ( int i = 0; i < iterations; i++ )
 		{
+			// Step 1 (calculate hidden variable values E[z_ij])
 			z1 = calculate_z_ij( data, u1, u2, sigma, z1 );
 			z2 = calculate_z_ij( data, u2, u1, sigma, z2 );
 
+			// Step 2 (calculate uj values)
 			u1 = calculate_u_j( data, z1 );
 			u2 = calculate_u_j( data, z2 );
 
+			// store and print results at each iteration
 			u1l[i] = u1;
 			u2l[i] = u2;
-			
 			System.out.printf( "Iteration: %d Mean 1: %.2f Mean 2: %.2f%n", i, u1, u2 );
 		}
 
@@ -70,26 +80,27 @@ public class Homework8
 		}
 		dataset.addSeries( "u2", seriesData2 );
 
-		JFreeChart chart2 = ChartFactory.createXYLineChart( "EM Algorithm", "Iteration", "Mean", dataset, PlotOrientation.VERTICAL, true, false, false );
+		JFreeChart chart2 = ChartFactory.createXYLineChart( "EM Algorithm Means", "Iteration", "Mean", dataset, PlotOrientation.VERTICAL, true, false, false );
 		ChartPanel chartPanel2 = new ChartPanel( chart2 );
 		JFrame frame2 = new JFrame( );
 		frame2.setSize( 1000, 1000 );
 		frame2.add( chartPanel2 );
 		frame2.setVisible( true );
 		
-
-		DefaultXYDataset dataset2 = new DefaultXYDataset( );
-
-		double[][] seriesData3 = new double[2][size];
+		HistogramDataset dataset2 = new HistogramDataset( );
+		
+		double[] seriesData3 = new double[size];
 		for ( int i = 0; i < size; i++ )
 		{
-			seriesData3[0][i] = data[i];
-			seriesData3[1][i] = Math.random( );
+			seriesData3[i] = data[i];
 		}
-		dataset2.addSeries( "data", seriesData3 );
-
+		dataset2.addSeries( "data", seriesData3, 60 );
+		
+//		XYLineAnnotation a1 = new XYLineAnnotation( u1, 0.0, u1, 1.0 );
+//		XYLineAnnotation a2 = new XYLineAnnotation( u2, 0.0, u2, 1.0 );
+		
 		//( title, xAxisLabel, yAxisLabel, dataset2, orientation, legend, tooltips, urls )
-		JFreeChart chart3 = ChartFactory.createScatterPlot( "Data", "Value", "", dataset2, PlotOrientation.VERTICAL, true, false, false );
+		JFreeChart chart3 = ChartFactory.createHistogram( "Data Histogram", "Value", "Relative Frequency", dataset2, PlotOrientation.VERTICAL, true, false, false );
 		ChartPanel chartPanel3 = new ChartPanel( chart3 );
 		JFrame frame3 = new JFrame( );
 		frame3.setSize( 1000, 1000 );
@@ -97,6 +108,7 @@ public class Homework8
 		frame3.setVisible( true );
 	}
 
+	// a helper function for loading data from the provided data file
 	public static double[] loadDataFile( String file ) throws IOException
 	{
 		BufferedReader in = new BufferedReader( new InputStreamReader( new FileInputStream( file ) ) );
@@ -122,6 +134,7 @@ public class Homework8
 		return dataArray;
 	}
 
+	// EM Algorithm step 1
 	public static double[] calculate_z_ij( double[] data, double u1, double u2, double sigma, double[] z )
 	{
 		for ( int i = 0; i < data.length; i++ )
@@ -135,6 +148,7 @@ public class Homework8
 		return z;
 	}
 
+	// EM Algorithm step 2
 	public static double calculate_u_j( double[] data, double[] z )
 	{
 		double numerator = 0.0;
@@ -152,6 +166,7 @@ public class Homework8
 		return numerator / denominator;
 	}
 
+	// calculate the normal cdf given a point x, a mean and a variance
 	public static double norm( double x, double u, double sigma )
 	{
 		double diff = x - u;
